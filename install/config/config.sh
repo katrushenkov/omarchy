2f6bbae413dfc7698d92e25dd51edc6b7bc3b673
Sergey Katrushenkov <katrushenkov@gmail.com>
2025-11-19 01:40:09 +1000

    update

--- install/config/config.sh
+++ install/config/config.sh
@@ -5,16 +5,14 @@ cp -R ~/.local/share/omarchy/config/* ~/.config/
 # Use default bashrc from Omarchy
 cp ~/.local/share/omarchy/default/bashrc ~/.bashrc
 
-mkdir -p ~/.local/src
 dotfilesrepo="https://github.com/katrushenkov/dotfiles.git"
 aurhelper="yay"
 repobranch="master"
 export TERM=ansi
-export name="ser"
 export branch="master"
-	export repodir="/home/$name/.local/src"
-	mkdir -p "$repodir"
-	chown -R "$name":wheel "$(dirname "$repodir")"
+export repodir="$HOME/.local/src"
+mkdir -p "$repodir"
+chown -R "$USER":wheel "$(dirname "$repodir")"
 rm -rf "$repodir/dotfiles"
 
 putgitrepo() {
@@ -22,13 +20,13 @@ putgitrepo() {
 	[ -z "$3" ] && branch="master" || branch="$repobranch"
 	dir=$(mktemp -d)
 	[ ! -d "$2" ] && mkdir -p "$2"
-	chown "$name":wheel "$dir" "$2"
-	sudo -u "$name" git -C "$repodir" clone --depth 1 \
+	chown "$USER":wheel "$dir" "$2"
+	sudo -u "$USER" git -C "$repodir" clone --depth 1 \
 		--single-branch --no-tags -q --recursive -b "$branch" \
 		--recurse-submodules "$1" "$dir"
-	sudo -u "$name" cp -rfT "$dir" "$2"
+	sudo -u "$USER" cp -rfT "$dir" "$2"
 }
 
-putgitrepo "$dotfilesrepo" "/home/$name" "$repobranch"
-rm -rf "/home/$name/README.md" "/home/$name/LICENSE" "/home/$name/FUNDING.yml"
-mv -f "/home/$name/.git/" "/home/$name/.local/src/dotfiles"
+putgitrepo "$dotfilesrepo" "$HOME" "$repobranch"
+rm -rf "$HOME/README.md" "$HOME/LICENSE" "$HOME/FUNDING.yml"
+mv -f "$HOME/.git/" "$repodir/dotfiles"
