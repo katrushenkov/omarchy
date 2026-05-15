23526dd0cf801b0d34d56ed43ee63804bffd2574
Sergey Katrushenkov <katrushenkov@gmail.com>
2026-05-15 15:31:51 +1000

    disable fcitx5 at autostart

--- default/hypr/autostart.lua
+++ default/hypr/autostart.lua
@@ -1,7 +1,7 @@
 o.launch_on_start("hypridle")
 o.launch_on_start("mako")
 o.exec_on_start("! omarchy-toggle-enabled waybar-off && " .. o.launch("waybar"))
-o.launch_on_start("fcitx5 --disable notificationitem")
+-- o.launch_on_start("fcitx5 --disable notificationitem")
 o.launch_on_start("swaybg -i ~/.config/omarchy/current/background -m fill")
 o.exec_on_start("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 o.exec_on_start("omarchy-first-run")
