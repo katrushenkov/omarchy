f366d0df09bdccf152d9f56fb4988418e3b8af05
Sergey Katrushenkov <katrushenkov@gmail.com>
2026-09-12 00:58:10 +1000

    install/packaging/base.sh: chore update nnn plugins

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -5,10 +5,9 @@ mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/omarchy-base.packages" | 
 
 omarchy-pkg-add "${packages[@]}"
 
-rm -rf $HOME/.config/nnn/plugins* >/dev/null
 # Install plugins for nnn file manager
+rm -rf $HOME/.config/nnn/plugins* >/dev/null
 sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
-
 mkdir -p $HOME/.cache/zsh
 
 mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/omarchy-aur.packages" | grep -v '^$')
